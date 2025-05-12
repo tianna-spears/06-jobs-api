9e397bd40d5796bc5c327a121583c735eadef9c9
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-05-12 21:34:04 +0100

    updated auth controller logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
-
     const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
         expiresIn: '30d',
     })
