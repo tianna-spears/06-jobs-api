f112138fdbcb5ac06d42f69d7b8314cc287fcae5
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-05-13 16:27:38 +0100

    completed Lesson 9 HW

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,11 @@
+// require Job model, status codes, and error codes
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
@@ -7,7 +13,9 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-    res.send('create job')
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -29,6 +30,17 @@ UserSchema.pre('save', async function () {
     this.password = await bcrypt.hash(this.password, salt)
 })
 
+UserSchema.methods.createJWT = function () {
+    return jwt.sign(
+        { userId: this._id, name: this.name}, 
+        process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME})
+}
 
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch;
+
+}
 
 module.exports = mongoose.model('User', UserSchema) 
